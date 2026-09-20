115ac60dc97e32032509da3d9efff5dc3645368f
Vibhu <121396471+vibhuawasthi30@users.noreply.github.com>
2026-09-20 17:06:03 -0400

    Add files via upload

---
+++ Day 1/file2.java
@@ -0,0 +1,14 @@
+class A
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.print("Java \t");
+        System.out.print("Programming\n");
+        System.out.println("\"I love Java\"");
+
+
+
+    }
+}
---
+++ Day 1/file3.java
@@ -0,0 +1,17 @@
+class A
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Name\t");
+        System.out.print("Age\t");
+        System.out.print("City\n");
+        System.out.print("Vibhu\t");
+        System.out.print("25\t");
+        System.out.print("Toronto\n");
+        System.out.print("John\t");
+        System.out.print("30\t");
+        System.out.print("New York\n");
+
+
+    }
+}
